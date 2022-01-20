c1b1ac52b45aa91cee90c195f87118154091b125
Maheswara Devo <pundadevo21@gmail.com>
2022-01-20 21:22:41 +0800

    add connection to database

---
+++ database/database_test.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_go_database")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
